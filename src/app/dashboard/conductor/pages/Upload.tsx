35994f5a777f85a1d14fb64120b9d31558680307
Ryan Nicolas M. Romero <109028966+arynn1@users.noreply.github.com>
2025-07-16 16:39:13 +0800

    FEAT: Implementing UI for the upload file page

--- src/app/dashboard/conductor/pages/Upload.tsx
+++ src/app/dashboard/conductor/pages/Upload.tsx
@@ -1,5 +1,23 @@
+import { Card } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const Upload = () => {
-  return <div>Upload</div>;
+  return (
+    <div className="">
+      <h1 className="text-2xl font-bold mb-2">Upload</h1>
+      <p className="mb-4 text-gray-600">Greetings Conductors! Please upload your file using the form below.</p>
+      <hr/>
+      <Input type="file" className="border border-gray-200 rounded-lg mb-4 mt-4" />
+      <hr/>
+      <Card className="h-[400px] w-full mt-4 bg-white shadow-md">
+        {/* Display file */}
+      </Card>
+      <Button className="w-full border border-gray-200 rounded-lg mb-4 mt-4">Submit</Button>
+    </div>
+  );
 };
 
 export default Upload;
+
